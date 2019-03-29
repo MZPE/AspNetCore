9e55dea556507d864257ea1cada3b66bb9d9a589
Javier Calvarro Nelson <jacalvar@microsoft.com>
2019-03-29 10:49:57 -0700

    Rebase + cleanup

--- src/Components/Server/test/Circuits/CircuitPrerendererTest.cs
+++ src/Components/Server/test/Circuits/CircuitPrerendererTest.cs
@@ -1,11 +1,17 @@
 // Copyright (c) .NET Foundation. All rights reserved.
 // Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
 
+using System;
+using System.IO;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Components.Server.Circuits;
+using Microsoft.AspNetCore.Components.Server.Prerendering;
 using Microsoft.AspNetCore.Components.Services;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
 using Moq;
 using Xunit;
 
@@ -13,6 +19,18 @@ namespace Microsoft.AspNetCore.Components.Server.Tests.Circuits
 {
     public class CircuitPrerendererTest
     {
+        // FOR NOW: The circuit prerenderer will wrap the content inside a div tag with a few data attributes
+        // data-circuit-id data-renderer-id data-component-id that are used for reconnecting to the running
+        // prerendered components.
+        // In the future we will lift this limitation by emiting comment nodes instead 
+        // <!-- M.A.C START circuit-id="..." renderer-id="..." component-id="..." --!> <!-- M.A.C END --!>
+        // Currently to render an interactive component or to prerender one and reconnect you need to provide a selector
+        // and an HTML tag in the document for the component to be plugged-in to, so we are not removing any existing capability
+        // by taking over the rendered element.
+        private static readonly Regex ContentWrapperRegex = new Regex(
+            "^<div data-circuit-id=\"[^\"]+\" data-renderer-id=\"0\" data-component-id=\"0\">(?<content>.*)</div>$",
+            RegexOptions.Compiled | RegexOptions.Singleline, TimeSpan.FromSeconds(1)); // Treat the entire input string as a single line
+
         // Because CircuitPrerenderer is a point of integration with HttpContext,
         // it's not a good candidate for unit testing. The majority of prerendering
         // unit tests should be elsewhere in HtmlRendererTests inside the
@@ -26,32 +44,40 @@ namespace Microsoft.AspNetCore.Components.Server.Tests.Circuits
         {
             // Arrange
             var circuitFactory = new TestCircuitFactory();
-            var circuitPrerenderer = new CircuitPrerenderer(circuitFactory);
-            var httpContext = new Mock<HttpContext>();
-            var httpRequest = new Mock<HttpRequest>().SetupAllProperties();
-            httpContext.Setup(h => h.Request).Returns(httpRequest.Object);
-            httpRequest.Object.Scheme = "https";
-            httpRequest.Object.Host = new HostString("example.com", 1234);
-            httpRequest.Object.Path = "/some/path";
+            var circuitRegistry = new CircuitRegistry(Options.Create(new CircuitOptions()), Mock.Of<ILogger<CircuitRegistry>>());
+            var circuitPrerenderer = new CircuitPrerenderer(circuitFactory, circuitRegistry);
+            var httpContext = new DefaultHttpContext();
+            var httpRequest = httpContext.Request;
+            httpRequest.Scheme = "https";
+            httpRequest.Host = new HostString("example.com", 1234);
+            httpRequest.Path = "/some/path";
 
             var prerenderingContext = new ComponentPrerenderingContext
             {
                 ComponentType = typeof(UriDisplayComponent),
                 Parameters = ParameterCollection.Empty,
-                Context = httpContext.Object
+                Context = httpContext
             };
 
             // Act
             var result = await circuitPrerenderer.PrerenderComponentAsync(prerenderingContext);
-
             // Assert
-            Assert.Equal(new[]
+            Assert.Equal(string.Join("", new[]
             {
                 "The current URI is ",
                 "https://example.com:1234/some/path",
                 " within base URI ",
                 "https://example.com:1234/"
-            }, result);
+            }), GetUnwrappedContent(result));
+        }
+
+        private string GetUnwrappedContent(ComponentPrerenderResult rawResult)
+        {
+            var writer = new StringWriter();
+            rawResult.WriteTo(writer);
+            return ContentWrapperRegex.Match(writer.ToString())
+                .Groups["content"].Value
+                .Replace("\r\n","\n");
         }
 
         [Fact]
@@ -59,33 +85,33 @@ namespace Microsoft.AspNetCore.Components.Server.Tests.Circuits
         {
             // Arrange
             var circuitFactory = new TestCircuitFactory();
-            var circuitPrerenderer = new CircuitPrerenderer(circuitFactory);
-            var httpContext = new Mock<HttpContext>();
-            var httpRequest = new Mock<HttpRequest>().SetupAllProperties();
-            httpContext.Setup(h => h.Request).Returns(httpRequest.Object);
-            httpRequest.Object.Scheme = "https";
-            httpRequest.Object.Host = new HostString("example.com", 1234);
-            httpRequest.Object.PathBase = "/my/dir";
-            httpRequest.Object.Path = "/some/path";
+            var circuitRegistry = new CircuitRegistry(Options.Create(new CircuitOptions()), Mock.Of<ILogger<CircuitRegistry>>());
+            var circuitPrerenderer = new CircuitPrerenderer(circuitFactory, circuitRegistry);
+            var httpContext = new DefaultHttpContext();
+            var httpRequest = httpContext.Request;
+            httpRequest.Scheme = "https";
+            httpRequest.Host = new HostString("example.com", 1234);
+            httpRequest.PathBase = "/my/dir";
+            httpRequest.Path = "/some/path";
 
             var prerenderingContext = new ComponentPrerenderingContext
             {
                 ComponentType = typeof(UriDisplayComponent),
                 Parameters = ParameterCollection.Empty,
-                Context = httpContext.Object
+                Context = httpContext
             };
 
             // Act
             var result = await circuitPrerenderer.PrerenderComponentAsync(prerenderingContext);
 
             // Assert
-            Assert.Equal(new[]
+            Assert.Equal(string.Join("", new[]
             {
                 "The current URI is ",
                 "https://example.com:1234/my/dir/some/path",
                 " within base URI ",
                 "https://example.com:1234/my/dir/"
-            }, result);
+            }), GetUnwrappedContent(result));
         }
 
         class TestCircuitFactory : CircuitFactory
@@ -96,7 +122,7 @@ namespace Microsoft.AspNetCore.Components.Server.Tests.Circuits
                 serviceCollection.AddScoped<IUriHelper>(_ =>
                 {
                     var uriHelper = new RemoteUriHelper();
-                    uriHelper.Initialize(uriAbsolute, baseUriAbsolute);
+                    uriHelper.InitializeState(uriAbsolute, baseUriAbsolute);
                     return uriHelper;
                 });
                 var serviceScope = serviceCollection.BuildServiceProvider().CreateScope();
