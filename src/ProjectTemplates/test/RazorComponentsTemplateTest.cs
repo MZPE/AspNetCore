b8e302ca8179e4286da9db2a3f7f4ef944d0ffa9
Javier Calvarro Nelson <jacalvar@microsoft.com>
2019-03-29 13:59:38 -0700

    Add additional map component hub overload and update templates

--- src/ProjectTemplates/test/RazorComponentsTemplateTest.cs
+++ src/ProjectTemplates/test/RazorComponentsTemplateTest.cs
@@ -23,7 +23,7 @@ namespace Templates.Test
 
         public Project Project { get; private set; }
 
-        [Fact(Skip = "https://github.com/aspnet/AspNetCore/issues/8244")]
+        [Fact]
         public async Task RazorComponentsTemplateWorksAsync()
         {
             Project = await ProjectFactory.GetOrCreateProject("razorcomponents", Output);
